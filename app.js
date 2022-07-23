0e926d575db44b95a16a930ad6849ef32b448b28
arun-177 <guleriaarun177@gmail.com>
2022-07-23 16:11:29 +0530

    last commit undo

--- app.js
+++ app.js
@@ -3,21 +3,20 @@ const bodyParser = require("body-parser");
 const route = require('./Routes/routing');
 
 const app = express();
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use('/', route);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', route);
 
 
-app.get('/', function (req, res) {
-  res.send('working')
-})
+
 
 
 const port = process.env.PORT || 3333;
-// app.set('port', port);
+app.set('port', port);
 
-// module.exports = app;
+module.exports = app;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
